c18b353a4262b0e5fce28c9001c71fc57f816274
Jan Weis <janweis@users.noreply.github.com>
2021-03-14 19:10:41 +0100

    ThreadException behoben

--- ProjektOrdner/Program.cs
+++ ProjektOrdner/Program.cs
@@ -1,10 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ProjektOrdner
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
             Application.EnableVisualStyles();
